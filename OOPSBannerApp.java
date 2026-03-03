b4740270fe5a8eb7119177c2cd7bf073074b7036
Aarav-12949 <goelaarav38@gmail.com>
2026-03-03 11:15:23 +0530

    Add files via upload

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,38 +1,8 @@
 public class OOPSBannerApp {
+
     public static void main(String[] args) {
-        String[] O = {
-                " ***** ",
-                "*     *",
-                "*     *",
-                "*     *",
-                "*     *",
-                "*     *",
-                " ***** "
-        };
-        String[] P = {
-                " ******",
-                "*     *",
-                "*     *",
-                " ******",
-                "*      ",
-                "*      ",
-                "*      "
-        };
-        String[] S = {
-                " ******",
-                "*      ",
-                "*      ",
-                " ***** ",
-                "      *",
-                "      *",
-                " ******"
-        };
-        String[] lines = new String[7];
-        for (int i = 0; i < 7; i++) {
-            lines[i] = String.join("   ", O[i], O[i], P[i], S[i]);
-        }
-        for (String line : lines) {
-            System.out.println(line);
-        }
+
+        System.out.println("OOPS");
+
     }
 }
